b19a071f68e4f96701b75ee30742b1b2276747ba
Aleksander Szewczak <aleksander.szewczak@inprox.pl>
2018-10-26 14:48:39 +0200

    lab3

---
+++ 0302/ConsoleApp2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03DniTygodnia
+{
+    class MonthsCollection
+    {
+        string[] months = { "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień" };
+        int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        private int getData(string month)
+        {
+            for(int i = 0; i < months.Length; i++)
+            {
+                if(months[i] == month)
+                {
+                    return days[i];
+                }
+            }
+            return -1;
+        }
+
+        private int find(string month)
+        {
+            for(int i = 0; i < months.Length; i++)
+            {
+                if(months[i] == month)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int this[string month]
+        {
+            get => getData(month);
+            set => days[find(month)] = value;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonthsCollection month = new MonthsCollection();
+            System.Console.WriteLine(month["Styczeń"]);
+            month["Styczeń"] = 15;
+            System.Console.WriteLine(month["Styczeń"]);
+            Console.ReadKey();
+        }
+    }
+}
